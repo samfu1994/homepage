3fbb01a89510067829a86bec51a9b9bc33c601c2
Hao Fu <hafu@amazon.com>
2019-06-05 16:27:49 -0700

    Updates

---
+++ precache-manifest.c41057ceb78be7ba2e4a3d90bb8c2785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857e4739f19c068b6f4cbfa91c860a22",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "67f0892e9ec11bab4873",
+    "url": "/homepage/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/homepage/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "67f0892e9ec11bab4873",
+    "url": "/homepage/static/js/main.03289b39.chunk.js"
+  },
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homepage/static/media/logo.5d5d9eef.svg"
+  }
+]);
